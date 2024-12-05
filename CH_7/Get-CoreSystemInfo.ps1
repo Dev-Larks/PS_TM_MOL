67c251086ce3f12bdac4d26532db75d471eed16f
Craig Larkin <craig.larkin@dpie.nsw.gov.au>
2024-12-05 16:42:34 +1100

    Add Get-CoreSystemInfo function

---
+++ CH_7/Get-CoreSystemInfo.ps1
@@ -0,0 +1,37 @@
+function Get-CoreSystemInfo {
+    [CmdletBinding()]
+    param (
+        [string[]] $ComputerName,
+
+        [string] $ErrorLog
+    )
+    
+    BEGIN {
+        
+    }
+    
+    PROCESS {
+        foreach ($computer in $ComputerName) {
+
+            $comp = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $computer
+            $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computer
+
+            $props = @{'Model'=$comp.Model;
+                       'ComputerName'=$comp.Name;
+                       'Manufacturer'=$comp.Manufacturer;
+                       'OSVersion'=$os.Version;
+                       'LastBootTime'=$os.LastBootUpTime}
+
+            $obj = New-Object -TypeName psobject -Property $props
+
+            Write-Output $obj
+        }
+    }
+    
+    END {
+        
+    }
+}
+
+Get-CoreSystemInfo -ComputerName localhost, two, three
+Get-CoreSystemInfo localhost x.txt
